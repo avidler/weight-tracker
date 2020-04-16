b1fa84f59611b063809b8ab3b500a5777501b155
avidler <a.vidler@live.co.uk>
2020-04-16 21:09:40 +0100

    fetch data from database and display

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom'
+import React, { useState } from 'react';
 import './App.css';
 
-import Login from './pages/Login'
+//import Login from './pages/Login'
 import User from './pages/User'
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [username, setUsername] = useState("") 
+
+  function handleChange(e) {
+    const {value} = e.target
+    setUsername(value)
+  }
+  
+  function submitName(e) {
+    e.preventDefault();
+    setIsLoggedIn(true)
+    
+  }
+
   return (
     <div className="App">
-        <Switch>
-        <Route exact path="/">
-                    <Login />
-        </Route>
-        <Route exact path="/user">
-                    <User />
-        </Route>
+      <h1>App Title</h1>
+      <h3>{isLoggedIn}</h3>
+        {isLoggedIn ? 
+        
+        <User username={username}/> 
+        : 
 
-        </Switch>
+      <div>
+        <h2>
+        Login or signup</h2>
+        <form onSubmit={submitName}>
+            <label>
+                <input type="text" value={username} onChange = {handleChange} />
+               
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
+  
+   
+</div>
+
+        }
     </div>
   );
 }
+
+
 
 export default App;
--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function User() {
+function User(props) {
+    const username = props.username
+    const [userWeights, setUserWeights] = useState([])
+    const [status, setStatus] = useState("Loading...")
+
+    
+    
+    useEffect(() => {
+        async function getUsers () {
+            await axios.get('/users')
+            .then((response) => {
+                //console.log(response.data);
+                //console.log(response.status);
+                //console.log(response.statusText);
+                //console.log(response.headers);
+                //console.log(response.config);
+                let data = response.data.filter((res, i) => (res.username===username));
+            
+            console.log("Fetched data: ", data)
+            console.log("Users weights: ",data[0].weights )
+            setUserWeights(data[0].weights)
+            setStatus("Loaded")
+            
+              });
+            
+        };
+        getUsers()
+    
+        },[username])
+
     return (
 <div>
-    <p>
-        user date
-    </p>
+
+        <h2>User Data for {username}</h2>
+        <div>
+    <div>Weights: {status}</div>
+    <div className="weightboard">
+            <h2>Weight History</h2>
+            <table className="weights">
+              <thead><tr><th>Date</th><th>Weight</th></tr></thead>
+              <tbody>
+            {userWeights.map((user) => 
+             <tr key={user.date}>
+                    <td id="post-date">{new Date(user.date).toDateString()} </td>
+                    <td id="post-weight"> {user.weight}</td>
+                </tr>
+            )}
+            </tbody>
+            </table>
+        </div>
+    </div>
 </div>
     )
 }
